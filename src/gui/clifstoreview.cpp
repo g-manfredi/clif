ded1933f6f67d855812d3767e5da87b08f7920d1
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2015-11-03 11:44:24 +0100

    nice async rendering for clifstoreview

--- src/gui/clifstoreview.cpp
+++ src/gui/clifstoreview.cpp
@@ -21,6 +21,7 @@ namespace clif {
 clifStoreView::clifStoreView(Datastore *store, QWidget* parent)
 : QWidget(parent)
 {
+  _store = store;
   _vbox = new QVBoxLayout(this);
   setLayout(_vbox);
   
@@ -28,21 +29,61 @@ clifStoreView::clifStoreView(Datastore *store, QWidget* parent)
   _vbox->addWidget(_view);
   
   _slider = new QSlider(Qt::Horizontal, this);
-  _slider->setMaximum(store->imgCount());
+  _slider->setTickInterval(1);
+  _slider->setTickPosition(QSlider::TicksBelow);
+  _slider->setMaximum(_store->imgCount()-1);
   _vbox->addWidget(_slider);
   
-  std::vector<int> n_idx(store->dims(),0);
-  n_idx[3] = 0;
-    
   _qimg = new QImage();
   
-  readQImage(store, n_idx, *_qimg, 0);
-  _view->setImage(*_qimg);
+  _show_idx = 0;
+  load_img();
+  
+  connect(_slider, SIGNAL(valueChanged(int)), this, SLOT(queue_sel_img(int)));
 }
 
 clifStoreView::~clifStoreView()
 {
   delete _qimg;
 }
+
+void clifStoreView::queue_sel_img(int n)
+{
+  _show_idx = n;
+
+  if (!_timer) {
+    _timer = new QTimer(this);
+    connect(_timer, SIGNAL(timeout()), this, SLOT(load_img()));
+    _timer->setSingleShot(true);
+    if (!_rendering)
+      _timer->start(0);
+  }
+}
+
+void clifStoreView::load_img()
+{
+  if (_timer)
+    _timer = NULL;
+  
+  if (_curr_idx == _show_idx)
+    return;
+  
+  std::vector<int> n_idx(_store->dims(),0);
+  n_idx[3] = _show_idx;
+  
+  readQImage(_store, n_idx, *_qimg, 0);
+  _view->setImage(*_qimg);
+  
+  _curr_idx = _show_idx;
+  
+  //force results of this slow operation to be displayed
+  _rendering = true;
+  qApp->processEvents();
+  _rendering = false;
+  
+  if (_timer)
+    _timer->start(0);
+}
+
 
 }
--- src/gui/clifstoreview.hpp
+++ src/gui/clifstoreview.hpp
@@ -7,6 +7,7 @@
 
 class QSlider;
 class QVBoxLayout;
+class QTimer;
 
 namespace clif {
   
@@ -22,11 +23,21 @@ public:
   clifStoreView(Datastore *store, QWidget* parent = NULL);
   virtual ~clifStoreView();
   
+private slots:
+  void load_img();
+  void queue_sel_img(int n);
+  
 private:
   QVBoxLayout *_vbox;
   clifScaledImageView *_view;
   QSlider *_slider;
   QImage *_qimg = NULL;
+  Datastore *_store = NULL;
+  int _curr_idx = -1;
+  int _show_idx = 0;
+  QTimer *_timer = NULL;
+  bool _rendering = false;
+  
 };
   
 }
