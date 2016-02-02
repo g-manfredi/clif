a55c406065fe16f55552801a38b02399b613a7e6
mgutsche <marcel.gutsche@iwr.uni-heidelberg.de>
2016-02-02 13:55:07 +0100

    added files

---
+++ src/lib/mat_helpers.hpp
@@ -0,0 +1,13 @@
+#ifndef _CLIF_M_HELPERS_H
+#define _CLIF_M_HELPERS_H
+
+#inclide "mat.hpp"
+
+namespace clif {
+  
+
+Void get_intensities(const Mat& m, Mat& intensities, int x, int y, float disp);
+
+}
+
+#endif
