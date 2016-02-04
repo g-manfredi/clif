9ead5e98136ab9b5f09ca3c22735acb5f49c5dfb
mgutsche <marcel.gutsche@iwr.uni-heidelberg.de>
2016-02-04 11:51:41 +0100

    changes for getting awesome brdf stuff done, WIP

--- src/lib/mat_helpers.cpp
+++ src/lib/mat_helpers.cpp
@@ -7,16 +7,16 @@ template<typename T> class get_intensities_dispatcher {
 public:
   void operator()(Mat* m, Mat* intensities, int x, int y, float disp)
   {
-    for(int v = 0; v<m[3];v++)
+    for(int v = 0; v<(*m)[3];v++)
     {
       float x_real = x + ((*m)[3]/2 - v)*disp;
       int x_l = x_real;
       int x_r = x_l + 1;
       float f = x_real - x_l;
-      if (x_l < 0 and x_r >= m[0])
+      if (x_l < 0 || x_r >= (*m)[0])
 	continue;
-      for (int c=0; c<3; c++ )	
-	intensities->operator()<T>(v,c) = m->operator()<T>(x_l, y, c, v)*(1-f) + m->operator()<T>(x_r, y, c, v)*f; 
+      for (int c=0; c<(*m)[2]; c++ )	
+	intensities->operator()<T>(c,v) = m->operator()<T>(x_l, y, c, v)*(1-f) + m->operator()<T>(x_r, y, c, v)*f; 
       
     }    
   }
